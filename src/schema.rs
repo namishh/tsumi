2f0a484c66572799783e2c5ba6ad069c7c82650c
quantinium03 <quant@quantinium.dev>
2025-06-12 11:26:39 +0530

    schemas

--- src/schema.rs
+++ src/schema.rs
@@ -1,48 +1,125 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    accounts (id) {
+        id -> Text,
+        userId -> Text,
+        #[sql_name = "type"]
+        type_ -> Text,
+        provider -> Text,
+        providerAccountId -> Text,
+        refresh_token -> Text,
+        access_token -> Text,
+        expires_at -> Timestamp,
+        token_type -> Nullable<Text>,
+        scope -> Nullable<Text>,
+        id_token -> Nullable<Text>,
+        session_state -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    email_verification_tokens (id) {
+        id -> Nullable<Text>,
+        token -> Text,
+        expires_at -> Timestamp,
+        userId -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    post_tags (post_id, tag_id) {
+        post_id -> Text,
+        tag_id -> Text,
+    }
+}
+
+diesel::table! {
     post_versions (id) {
         id -> Nullable<Text>,
-        post_id -> Nullable<Text>,
+        post_id -> Text,
+        userId -> Text,
+        title -> Text,
         content -> Text,
-        commit_message -> Nullable<Text>,
-        version_number -> Integer,
-        created_at -> Nullable<Text>,
+        description -> Text,
+        commit_hash -> Text,
+        commit_message -> Text,
+        created_at -> Timestamp,
     }
 }
 
 diesel::table! {
     posts (id) {
         id -> Nullable<Text>,
-        user_id -> Nullable<Text>,
-        slug -> Text,
+        userId -> Text,
         title -> Text,
+        description -> Text,
+        slug -> Text,
         content -> Text,
-        is_published -> Nullable<Integer>,
-        created_at -> Nullable<Text>,
-        updated_at -> Nullable<Text>,
+        is_published -> Bool,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    refresh_tokens (id) {
+        id -> Nullable<Text>,
+        token -> Text,
+        userId -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    reset_tokens (id) {
+        id -> Nullable<Text>,
+        token -> Text,
+        expires_at -> Timestamp,
+        userId -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    tags (id) {
+        id -> Nullable<Text>,
+        name -> Text,
     }
 }
 
 diesel::table! {
     users (id) {
-        id -> Nullable<Text>,
+        id -> Text,
         username -> Text,
         email -> Text,
-        password_hash -> Nullable<Text>,
-        github_id -> Nullable<Text>,
-        google_id -> Nullable<Text>,
-        custom_css -> Nullable<Text>,
-        theme -> Nullable<Text>,
-        created_at -> Nullable<Text>,
+        password -> Text,
+        email_verified -> Bool,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        deleted_at -> Nullable<Timestamp>,
     }
 }
 
+diesel::joinable!(accounts -> users (userId));
+diesel::joinable!(email_verification_tokens -> users (userId));
+diesel::joinable!(post_tags -> posts (post_id));
+diesel::joinable!(post_tags -> tags (tag_id));
 diesel::joinable!(post_versions -> posts (post_id));
-diesel::joinable!(posts -> users (user_id));
+diesel::joinable!(post_versions -> users (userId));
+diesel::joinable!(posts -> users (userId));
+diesel::joinable!(refresh_tokens -> users (userId));
+diesel::joinable!(reset_tokens -> users (userId));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    accounts,
+    email_verification_tokens,
+    post_tags,
     post_versions,
     posts,
+    refresh_tokens,
+    reset_tokens,
+    tags,
     users,
 );
